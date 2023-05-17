34b6578bf8e33b85e98ac827c5d453d79b8c49d7
Giiyasa <makgilang90@gmail.com>
2023-05-17 14:23:55 +0700

    update logo

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -12,18 +12,24 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
+
             <div class="col-lg-7">
+
+
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg">
-                                <div class="p-5">
+                        <div class="row align-items-end justify-content-center ">
+                            <img src="<?= base_url('assets/logo.png'); ?>" alt="Logo" height="150">
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-lg-10">
+                                <div class="p-1">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">POSPEN LOGIN</h1>
+                                        <h1 class="h4 text-gray-900 mb-4 mt-4">POSPEN LOGIN</h1>
                                     </div>
                                     <?= $this->session->flashdata('message'); ?>
-                                    <form class="user" method="POST" action="<?= base_url('auth'); ?>">
+                                    <form class="user mx-auto" method="POST" action="<?= base_url('auth'); ?>">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
                                             <?= form_error('email', '<small class="text-danger pl-3">', '</small>');  ?>
@@ -46,10 +52,12 @@
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </body>
 
