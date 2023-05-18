0a15a6e50107534c3d1d8d8286f3045fca98eb9b
Giiyasa <makgilang90@gmail.com>
2023-05-18 16:54:44 +0700

    logout plus icon user dropdown

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,11 +7,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-    crossorigin="anonymous">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-jSNXwXpPf+kP5K7M1jpJy9j/UgBj+lkgje7jwdd2u+wCm3pBsl0SfuD58iKuAvLEhQqF1c+f8yW2XJ38OzUK1Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <!-- My CSS -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
 
   <title><?php echo $judul; ?>
   </title>
@@ -19,21 +19,27 @@
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-     <img src="<?= base_url(); ?>assets/logo.png" style="width:40px;height:40px>
-    <div class="container">
+    <img src="<?= base_url(); ?>assets/logo.png" style="width:40px;height:40px>
+    <div class=" container">
 
-      <a class="navbar-brand" href="#">POSPEN</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link" href="<?= base_url(); ?>home">Home
-            <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="<?= base_url(); ?>pensiunan">Pensiunan</a>
-          <a class="nav-item nav-link" href="#">About</a>
+    <a class="navbar-brand" href="#">POSPEN</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav mr-auto">
+        <a class="nav-item nav-link" href="<?= base_url(); ?>home">Home<span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="<?= base_url(); ?>pensiunan">Pensiunan</a>
+        <a class="nav-item nav-link" href="#">About</a>
+      </div>
+      <div class="dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" id="logoutDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="logoutDropdown">
+          <a class="dropdown-item" href="<?= base_url('logout'); ?>">Logout</a>
         </div>
       </div>
+    </div>
     </div>
   </nav>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,67 @@
-const flashData = $('.flash-data').data('flashdata');
+const flashData = $(".flash-data").data("flashdata");
 
 if (flashData) {
-    Swal({
-        title: 'Data Pensiun',
-        text: 'Berhasil ' + flashData,
-        type: 'success'
-    });
+  Swal({
+    title: "Data Pensiun",
+    text: "Berhasil " + flashData,
+    type: "success",
+  });
 }
 
 // tombol-hapus
-$('.tombol-hapus').on('click', function (e) {
+$(".tombol-hapus").on("click", function (e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
 
-    e.preventDefault();
-    const href = $(this).attr('href');
+  Swal({
+    title: "Apakah anda yakin",
+    text: "data pensiun akan dihapus",
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Hapus Data!",
+  }).then((result) => {
+    if (result.value) {
+      document.location.href = href;
+    }
+  });
+});
 
-    Swal({
-        title: 'Apakah anda yakin',
-        text: "data pensiun akan dihapus",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Hapus Data!'
-    }).then((result) => {
-        if (result.value) {
-            document.location.href = href;
-        }
-    })
+document.addEventListener("DOMContentLoaded", function (event) {
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId);
 
+    // Validate that all variables exist
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener("click", () => {
+        // show navbar
+        nav.classList.toggle("show");
+        // change icon
+        toggle.classList.toggle("bx-x");
+        // add padding to body
+        bodypd.classList.toggle("body-pd");
+        // add padding to header
+        headerpd.classList.toggle("body-pd");
+      });
+    }
+  };
+
+  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll(".nav_link");
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
+    }
+  }
+  linkColor.forEach((l) => l.addEventListener("click", colorLink));
+
+  // Your code to run since DOM is loaded and ready
 });
